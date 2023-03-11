712b9201ebfca001065ff7f4f136111ee5f3fbfd
Karla Agraz <karla.agraz@gmail.com>
2023-03-11 00:51:45 -0500

    ADD: game setup

---
+++ assets/js/juego.js
@@ -0,0 +1,42 @@
+// 2C = Two of Clubs
+// 2D = Two of Diamonds
+// 2H = Two of Hearts
+// 2S = Two of Spades
+
+let deck = [];
+const tipos = ["C", "D", "H", "S"];
+const especiales = ["A", "J", "Q", "K"];
+
+// Esta función crea un nuevo deck
+const crearDeck = () => {
+  for (let i = 2; i <= 10; i++) {
+    for (let tipo of tipos) {
+      deck.push(i + tipo);
+    }
+  }
+
+  for (let tipo of tipos) {
+    for (let esp of especiales) {
+      deck.push(esp + tipo);
+    }
+  }
+
+  deck = _.shuffle(deck);
+  console.log(deck);
+  return deck;
+};
+
+crearDeck();
+
+//Esta función me permite tomar una carta
+const pedirCarta = () => {
+  if (deck.length === 0) {
+    throw "No hay cartas en el deck";
+  }
+
+  const carta = deck.pop();
+
+  console.log(deck);
+  console.log(carta);
+  return carta;
+};
